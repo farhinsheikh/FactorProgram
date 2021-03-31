747f42910c28fa66fb7e332741c4c767b645b6be
farhinsheikh <farhinsheikh2015@gmail.com>
2021-03-31 21:50:21 +0530

    [Farhin] add : factor program

---
+++ Factor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeFactorProgram
+{
+    //created class Factor
+    class Factor
+    {
+        //PrimeFactors() method to find prime factors
+        public static void PrimeFactors()
+        {
+            Console.WriteLine("Enter the number");
+            // converting string value to int
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Factors=");
+            // for loop iterates to the given number
+            for (int i = 2; i * i <= number; i++)
+            {
+                // checking for perfect divisible value
+                while (number % i == 0)
+                {                       
+                    Console.Write(i + " ");
+
+                    number = number / i;
+                }
+            }
+            //// if number is prime and above 1 then it types the number 
+            if (number > 1)
+            {
+                Console.Write(number);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeFactorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factor.PrimeFactors();
+            Console.Read();
+        }
+    }
+}
